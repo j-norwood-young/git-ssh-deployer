4541dfb8e415a3c31608b35a40a5da5a46681117
Jason Norwood-Young <jason@10layer.com>
2019-08-29 13:45:27 +0200

    Some testing stuff

---
+++ index.js
@@ -0,0 +1,30 @@
+const config = require("config");
+const ssh = require("node-ssh");
+
+const http = require('http');
+
+const handleHook = data => {
+    console.log(data);
+}
+
+const httpServer = (req, res) => {
+    let data = []
+    req.on('data', chunk => {
+        data.push(chunk)
+    });
+    req.on('end', () => {
+        try {
+            const completed_data = JSON.parse(data);
+            handleHook(completed_data);
+        } catch(err) {
+            console.error("Error parsing data", err);
+        }
+    })
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`Got it, thanks!`);
+    res.end();
+}
+
+const app = http.createServer(httpServer);
+
+app.listen(3000);
